92706ae83d328b6b19504b7b19761678db15b375
Bradjo555 <72301431+Bradjo555@users.noreply.github.com>
2023-12-31 18:35:54 +0100

    final update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import { type Component } from "solid-js"
 import Navbar from "../components/Navbar";
 import { TbBrandSolidjs, TbBrandFirebase, TbBrandVercel  } from 'solid-icons/tb'
+import { A } from "@solidjs/router";
 
 const Home: Component = () => {
     return (
         <div>
-            <h1 class="text-4xl text-center">Home</h1>
+            <h1 class="text-4xl font-black text-center">Solid Authentication</h1>
+            <div class="flex justify-center my-5">
+                <A role="button" href="/register" class="w-max bg-[#E61046] px-5 py-3 font-bold text-light rounded-md hover:opacity-70 transition-all active:scale-95">Get started</A>
+            </div>
             <div>
                 <p class="text-center mt-12 text-xl font-Epilogue">Made using Solidjs and Firebase then deployed to Netlify.</p>
             </div>
